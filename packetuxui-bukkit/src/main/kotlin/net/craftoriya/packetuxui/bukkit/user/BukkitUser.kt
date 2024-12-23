0290e325e4db9ec125ade0ec3ab02b9e884aed01
Ammo <ammo@slne.dev>
2024-12-23 05:29:59 +0100

    chore: Implement hashCode and equals for user to be used in sets

--- packetuxui-bukkit/src/main/kotlin/net/craftoriya/packetuxui/bukkit/user/BukkitUser.kt
+++ packetuxui-bukkit/src/main/kotlin/net/craftoriya/packetuxui/bukkit/user/BukkitUser.kt
@@ -1,12 +1,12 @@
 package net.craftoriya.packetuxui.bukkit.user
 
 import com.github.retrooper.packetevents.PacketEvents
-import com.github.retrooper.packetevents.protocol.player.User as PacketUser
 import net.craftoriya.packetuxui.bukkit.extensions.toPlayer
 import net.craftoriya.packetuxui.user.User
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 import java.util.*
+import com.github.retrooper.packetevents.protocol.player.User as PacketUser
 
 class BukkitUser(override val uuid: UUID) : User {
 
@@ -23,4 +23,19 @@ class BukkitUser(override val uuid: UUID) : User {
     override fun sendMessage(message: Component) {
         player?.sendMessage(message)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BukkitUser
+
+        return uuid == other.uuid
+    }
+
+    override fun hashCode(): Int {
+        return uuid.hashCode()
+    }
+
+
 }
