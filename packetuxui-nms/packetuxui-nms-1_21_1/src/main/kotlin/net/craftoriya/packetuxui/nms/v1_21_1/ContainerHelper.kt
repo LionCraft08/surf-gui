8321f0e131c169fa1f5b0571eb416bfe253751cf
twisti <denizgebauer@web.de>
2024-12-23 17:46:22 +0100

    add nms extensions

--- packetuxui-nms/packetuxui-nms-1_21_1/src/main/kotlin/net/craftoriya/packetuxui/nms/v1_21_1/ContainerHelper.kt
+++ packetuxui-nms/packetuxui-nms-1_21_1/src/main/kotlin/net/craftoriya/packetuxui/nms/v1_21_1/ContainerHelper.kt
@@ -3,22 +3,13 @@ package net.craftoriya.packetuxui.nms.v1_21_1
 import net.craftoriya.packetuxui.nms.common.ContainerHelper
 import net.craftoriya.packetuxui.user.User
 import org.bukkit.Bukkit
-import org.bukkit.craftbukkit.entity.CraftPlayer
 
+@Suppress("ClassName")
 object ContainerHelper1_21_4 : ContainerHelper {
 
-    override fun getNextContainerId(user: User): Int {
-        val player = Bukkit.getPlayer(user.uuid) ?: return -1
-        val craftPlayer = player as CraftPlayer
+    override fun getNextContainerId(user: User) =
+        Bukkit.getPlayer(user.uuid)?.toNms()?.nextContainerCounter() ?: -1
 
-        return craftPlayer.handle.nextContainerCounter()
-    }
-
-    override fun hasOpenedContainer(user: User): Boolean {
-        val player = Bukkit.getPlayer(user.uuid) ?: return false
-        val craftPlayer = player as CraftPlayer
-
-        return craftPlayer.handle.hasContainerOpen()
-    }
-
+    override fun hasOpenedContainer(user: User) =
+        Bukkit.getPlayer(user.uuid)?.toNms()?.hasContainerOpen() == true
 }
