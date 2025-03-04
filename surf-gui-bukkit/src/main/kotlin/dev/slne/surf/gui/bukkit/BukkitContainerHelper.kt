71abb12ed37ef1e35343757b0bff1d21aae4acc8
twisti <denizgebauer@web.de>
2025-03-04 19:35:31 +0100

    refactor: optimize container helper retrieval with lazy initialization

--- surf-gui-bukkit/src/main/kotlin/dev/slne/surf/gui/bukkit/BukkitContainerHelper.kt
+++ surf-gui-bukkit/src/main/kotlin/dev/slne/surf/gui/bukkit/BukkitContainerHelper.kt
@@ -1,6 +1,5 @@
 package dev.slne.surf.gui.bukkit
 
-import dev.slne.surf.gui.nms.common.ContainerHelper
 import dev.slne.surf.gui.nms.v1_21_1.ContainerHelper1_21_1
 import dev.slne.surf.gui.nms.v1_21_4.ContainerHelper1_21_4
 import dev.slne.surf.gui.user.User
@@ -8,22 +7,21 @@ import org.bukkit.Bukkit
 
 object BukkitContainerHelper {
 
-    private fun getContainerHelper(): ContainerHelper {
-        val serverVersion = Bukkit.getMinecraftVersion()
-
-        return when {
-            serverVersion.contains("1.21.1") -> ContainerHelper1_21_1
-            serverVersion.contains("1.21.4") -> ContainerHelper1_21_4
-            else -> error("Unsupported server version: $serverVersion")
+    private val containerHelper by lazy {
+        val v = Bukkit.getMinecraftVersion()
+        when {
+            v.contains("1.21.1") -> ContainerHelper1_21_1
+            v.contains("1.21.4") -> ContainerHelper1_21_4
+            else -> error("Unsupported server version: $v")
         }
     }
 
     fun getNextContainerId(user: User): Int {
-        return getContainerHelper().getNextContainerId(user)
+        return containerHelper.getNextContainerId(user)
     }
 
     fun hasOpenedContainer(user: User): Boolean {
-        return getContainerHelper().hasOpenedContainer(user)
+        return containerHelper.hasOpenedContainer(user)
     }
 
 }
